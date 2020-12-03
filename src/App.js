d911bcf89d06f7667068c00e21a9a9866d2840b2
spencermacdonald <macdonaldspencer1@gmail.com>
2020-12-03 18:16:44 -0400

    Added more content

--- src/App.js
+++ src/App.js
@@ -12,14 +12,29 @@ function App() {
           <h1 className="title"><u>Mersenne Prime</u></h1>
           <p className= "paragraph">What is a Mersenne Prime? Well a mersenne prime is a prime number that can be written in the form:</p>
           <img src={equation} alt="M = 2^n -1"/>
-          <p className= "paragraph">Where n is an integer that makes the RHS equation equal to the left.
+          <p className= "paragraph">Where n is an integer that makes the RHS equation equal to the prime number on the LHS.
             These primes were named after french mathmatician Marin Mersenne, who spent his life researching them.
           </p>
           <h3 className="title"><u>More facts about Mersenne Primes</u></h3>
           <ul>
             <li>
               <p className= "paragraph">
-                The largest known Mersenne prime was found on <a href="https://www.mersenne.org/report_exponent/?exp_lo=82589933">Dec.07/2018</a>, which was found to be n = 82,589,933.
+                The largest currently known (and verified) Mersenne prime was found on <a href="https://www.mersenne.org/report_exponent/?exp_lo=82589933">Dec.07/2018</a>, which was found to be n = 82,589,933.
+                This is the M51 prime number.
+              </p>
+            </li>
+            <li>
+              <p className= "paragraph">
+                The <a href="https://www.mersenne.org/">GIMPS</a> is a project that revolves around using the combined power of thousands of computers that are
+                run in parrallel to both find and verify new Mersenne primes. This is done through running a <a href="https://www.mersenne.org/download/">custom software</a> developed by the project.
+              </p>
+            </li>
+          </ul>
+          <h3 className="title"><u>Relating back to Math 3093</u></h3>
+          <ul>
+            <li>
+              <p className= "paragraph">
+                We studied primes over the entirety of the course. From the knowledge gained from class, we know that a prime number 'p' is prime if gcd(p,a) for any integer 1 &lt; a &lt; p.
               </p>
             </li>
             <li>
@@ -28,8 +43,9 @@ function App() {
               </p>
             </li>
           </ul>
-          <p><a href="https://oeis.org/A000043"> Read more about it on oeis</a></p>
         </div>
+
+
         <div className="main">
           <hr/>
           <h2 id= "header2"><u>Try it for yourself</u></h2>
@@ -47,6 +63,7 @@ function App() {
           <ul>
             <li id="references-text"><a href="https://oeis.org/A000043"> Mersenne Primes Oeis</a></li>
             <li id="references-text"><a href="https://www.mersenne.org/primes/"> History of Mersenne primes / known primes</a></li>
+            <li id="references-text"><a href="https://www.mersenne.org/#:~:text=GIMPS%2C%20the%20Great%20Internet%20Mersenne%20Prime%20Search%2C%20was%20formed%20in,%22needles%20in%20a%20haystack%22.">History of GIMPS</a></li>
           </ul>
         </div>
       </header>
